2fd032f0eb950f843fed6d5a28e2afc1ec7290cf
Hardwaregore <hardwaregore101@gmail.com>
2022-11-11 00:12:55 +0000

    Added time

--- home.php
+++ home.php
@@ -5,16 +5,19 @@
 		<script defer src='script.js'></script>
 		<link rel='stylesheet' href='style.css'></link>
 		<link rel="shortcut icon" href="icons/logo.png" type="image/x-icon"></link>
+		<script defer src="time.js"></script>
 	</head>
 
 
 	<body>
 		<nav>
 			<a href="home.php"><img src="icons/logo.png" alt="logo" height="50" width="50"></img></a>
+			<p id="demo" class="isBlack"></p>
 			<div class='links'>
 				<a onclick="confLogout()"><img src='icons/logout_icon.png' alt='logout' height="50" width="50"></a>
 			</div>
 		</nav>
+
 	</body>
 
 </html>
