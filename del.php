93fd903219325d5e99e3083881bce766a5e00489
Hardwaregore <hardwaregore101@gmail.com>
2023-01-10 14:43:23 -0600

    Fixed sql statements

--- del.php
+++ del.php
@@ -11,11 +11,10 @@
     }
 
 
-        $username = $_SESSION['username'];
 
 require_once "connect.php";
 $rem = $_GET['rem'];
-$sql = "DELETE FROM `reminders` WHERE name = '$rem' AND username = '$username';";
+$sql = "DELETE FROM `reminders` WHERE name = '$rem';";
 $result = mysqli_query($conn, $sql);
 header('Location: home.php');
 ?>
--- unmark.php
+++ unmark.php
@@ -9,10 +9,9 @@ header('Location: home.php');
 }
 
 
-
 require_once "connect.php";
 $rem = $_GET['rem'];
-$sql = "UPDATE `reminders` SET completed = 1 WHERE name = '$rem';";
+$sql = "UPDATE `reminders` SET completed = 0 WHERE name = '$rem';";
 $result = mysqli_query($conn, $sql);
 header('Location: home.php');
 ?>
